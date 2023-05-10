98a06979eb4d594572aa7f71bd4e1d3026f44db0
Stas Ukolov <ukoloff@gmail.com>
2023-05-10 19:04:58 +0500

    Select priorities

--- src/end0/query.js
+++ src/end0/query.js
@@ -6,5 +6,21 @@ const metal = require('./metal')
 module.exports = query
 
 function query(m) {
-  cur_metal = metal(m)
+  var cur_metal = metal(m)
+  for (const grp of prioritets(m)) {
+    m.Kd_gr_rezc = grp
+  }
+}
+
+function prioritets(m) {
+  var ps = db.prioritet.filter(x => x.KTE == m.cur_kte && x.CHIST == m.cur_chist)
+  if (ps.length != 1) {
+    m.errors.push("Error looking for priority ${m.cur_kte}:${m.cur_chist}")
+    return
+  }
+  ps = ps[0]
+  return Object.keys(ps)
+    .filter(x => /^pri\D*\d+$/i.test(x))
+    .map(x => ps[x])
+    .filter(x => x)
 }
