6b52f2b7a43dce506dfc9864bd44d8183ab45793
Stas Ukolov <ukoloff@gmail.com>
2023-05-25 13:52:30 +0500

    Run tests

--- test/turret.js
+++ test/turret.js
@@ -1,15 +1,27 @@
 const fs = require('fs')
 const path = require('path')
+const yaml = require('js-yaml')
 
-describe("Find tool",  context)
+describe("Find tool", context)
 
 function context() {
   let folder = path.join(path.dirname(__filename), 'kte')
   let d = fs.opendirSync(folder)
   let f
-  while(f = d.readSync()) {
-    if(!f.isFile() || path.extname(f.name).toLowerCase() != '.yml') continue
+  let ktes = []
+  while (f = d.readSync()) {
+    if (!f.isFile() || path.extname(f.name).toLowerCase() != '.yml') continue
     let z = path.join(folder, f.name)
-    console.log(z)
+    let kte  = yaml.load(fs.readFileSync(z, 'utf-8'))
+    kte.name = kte.name || path.parse(f.name).name
+    ktes.push(kte)
   }
+  const only = ktes.filter(x => 'only' in x).length > 0
+  ktes.forEach(kte => {
+    it(kte.name, function() {
+      if ('skip' in kte) this.skip()
+      if (only && !('only' in kte)) this.skip()
+      // TODO: kte
+    })
+  })
 }
