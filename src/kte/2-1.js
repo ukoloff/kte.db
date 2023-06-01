a9e939c25ef427d297b51eb3d3be994d11711ad2
Stas Ukolov <ukoloff@gmail.com>
2023-06-01 13:44:53 +0500

    Start KTE-2+

---
+++ src/kte/2-1.js
@@ -0,0 +1,11 @@
+// * MESSAGEBOX("Открытая зона наружная чистовая ",0," ")
+const orderBy = require('lodash/orderBy')
+const round = require('lodash/round')
+
+const db = require('../db')
+
+module.exports = query
+
+function query(m) {
+
+}
